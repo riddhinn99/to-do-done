7fa209f8556a12fae0fb124b7439cef05aef22d8
RiddhiNN <riddhinn@gmail.com>
2020-04-29 22:05:32 +0530

    Updates

---
+++ precache-manifest.f6ba0fdeb42d112c814564eb13475736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54ee6290e4e2c90770d07a657b932f6",
+    "url": "/to-do-done/index.html"
+  },
+  {
+    "revision": "72736eb28c0a3479ea6d",
+    "url": "/to-do-done/static/css/main.b2351c59.chunk.css"
+  },
+  {
+    "revision": "3c781481c2537370b9c5",
+    "url": "/to-do-done/static/js/2.dd797394.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/to-do-done/static/js/2.dd797394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72736eb28c0a3479ea6d",
+    "url": "/to-do-done/static/js/main.a79276a9.chunk.js"
+  },
+  {
+    "revision": "fe32847d386b3eab24b2",
+    "url": "/to-do-done/static/js/runtime-main.e1b3c4f2.js"
+  }
+]);
